fd30caf0c5fa0d6d23e30f6a664a4d026f3af643
Frankie Liu <frankie.liu@oracle.com>
2018-01-23 18:29:44 -0800

    Added Spark

---
+++ Spark/Apache/env.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/home/fyliu/jdk1.8.0_161
+spark=/import/async/cad/2017/Spark/spark/bin
+import=/import/async/cad/2017/Maven/maven/bin
+pathprepend $import:$spark
+
+# export PATH=$import:$PATH
---
+++ Spark/Chapter01/env.sh
@@ -0,0 +1,5 @@
+spark=/import/async/cad/2017/Spark/spark/bin
+import=/import/async/cad/2017/Maven/maven/bin
+pathprepend $import:$spark
+
+# export PATH=$import:$PATH
