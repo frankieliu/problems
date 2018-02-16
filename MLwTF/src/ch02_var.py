39d0e1f71ab3d611229c520d2eb5cd5bce75a31d
BuildTools <unconfigured@null.spigotmc.org>
2018-02-16 00:25:58 -0800

    var

--- MLwTF/src/ch02_var.py
+++ MLwTF/src/ch02_var.py
@@ -1,1 +1,18 @@
-import tensorflow
+import tensorflow as tf
+ts = tf.InteractiveSession()
+
+r = [1,2,8,-1,0,5.5,6,13]
+
+s = tf.Variable(False)
+s.initializer.run()
+
+for i in range(1, len(r)):
+    
+    if r[i] - r[i-1] > 5:
+        updater = tf.assign(s, tf.constant(True))
+        updater.eval()
+    else:
+        tf.assign(s, False).eval()
+        
+    print("Spike",s.eval())
+    
