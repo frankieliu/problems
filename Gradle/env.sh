fc7fc6d55a7c8fb625ec7e799f83833aa31fe969
Frankie Liu <frankie.liu@oracle.com>
2018-05-15 19:09:58 -0700

    Adding env gradle_home and scala example

--- Gradle/env.sh
+++ Gradle/env.sh
@@ -1,1 +1,3 @@
-pathappend /import/async/cad/2018/gradle/gradle-4.7/bin
+export GRADLE_HOME=/import/async/cad/2018/gradle/gradle-4.7
+pathappend $GRADLE_HOME/bin
+export JAVA_HOME=/import/async/cad/linux/jvm/java-8-oracle/
--- Gradle/scala/src/main/scala/Main.scala
+++ Gradle/scala/src/main/scala/Main.scala
@@ -1,3 +1,3 @@
 object Main extends App {
-  println("Hello, world")
+  println("Hello, world");
 }
