13e8a90e4ea8d5be6626a5df42f1c7c5729a241c
Frankie Liu <frankie.y.liu@gmail.com>
2018-10-12 10:51:57 -0700

    Angular and Vue

---
+++ Angular/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+    { path: 'heroes',
+      component: HeroesComponent }
+];
+
+@NgModule({
+    imports: [
+      // CommonModule
+        RouterModule.forRoot(routes)
+    ],
+    // declarations: []
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
